21da3677ee17b1b46c5a1ec9cb26d523b0bc0efa
linhui <marvin.huang1017@gmail.com>
2024-02-14 16:44:04 +0800

    d

--- crestbot.sh
+++ crestbot.sh
@@ -4,9 +4,8 @@ docker run -it --rm \
 -v "$PWD/html/certbot:/var/www/certbot" \
 -v "/var/run/docker.sock:/var/run/docker.sock:ro" \
 certbot/certbot certonly \
---webroot --webroot-path=/var/www/certbot \
---email s9043044@gmail.com --agree-tos --no-eff-email \
 --manual --preferred-challenges=dns \
+--email s9043044@gmail.com --agree-tos --no-eff-email \
 -d '*.chinureeves.com' -d chinureeves.com
 
 
