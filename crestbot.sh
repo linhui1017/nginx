16665f660412faaeacb125ee980a9c7a4fb44679
linhui <marvin.huang1017@gmail.com>
2024-02-14 15:45:25 +0800

    update

---
+++ crestbot.sh
@@ -0,0 +1,9 @@
+docker run -it --rm \
+-v "$PWD/certs/letsencrypt:/etc/letsencrypt" \
+-v "$PWD/certs/lib/letsencrypt:/var/lib/letsencrypt" \
+-v "$PWD/html/certbot:/var/www/certbot" \
+-v "/var/run/docker.sock:/var/run/docker.sock:ro" \
+certbot/certbot certonly \
+--webroot --webroot-path=/var/www/certbot \
+--email s9043044@gmail.com --agree-tos --no-eff-email \
+-d chinureeves.com -d ubuntu.chinureeves.com
