f59738d3097350da5516df71394e3fb9606769ba
linhui <marvin.huang1017@gmail.com>
2024-02-14 14:53:24 +0800

    update

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ docker rm nginx
 docker run --name nginx -d \
   -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf \
   -v $PWD/conf/conf.d:/etc/nginx/conf.d:rw \
+  -v $PWD/certs:/etc/nginx/certs:rw \
   -v $PWD/conf/mime.types:/etc/nginx/mime.types \
   -v $PWD/html:/usr/share/nginx/html:rw \
   -v $PWD/log:/var/log/nginx:rw \
   --restart=always \
   --dns 8.8.8.8 \
-  -p 8080:80 \
+  -p 80:80 \
   -t nginx:latest
 
 
